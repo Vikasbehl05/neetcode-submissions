d6f75b7e6df636f6ab9a21b9b33882937c0ac472
Vikasbehl05 <88477915+Vikasbehl05@users.noreply.github.com>
2026-06-24 22:05:07 +0530

    Add: plus-one - submission-4

---
+++ Data Structures & Algorithms/plus-one/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    // public int power(int num, int power)
+    // {
+    //     int result = 1;
+    //     while (power != 0){
+    //         result = result * num;
+    //         power--;
+    //     }
+
+    // }
+
+    public int[] plusOne(int[] digits) {
+        for (int j = digits.length - 1; j >= 0; j--) {
+            if (digits[j] < 9) {
+                digits[j]++;
+                return digits;
+            }
+            digits[j] = 0;
+        }
+
+        int[] newdigits = new int[digits.length + 1];
+        newdigits[0] = 1;
+        return newdigits;
+    }
+}
