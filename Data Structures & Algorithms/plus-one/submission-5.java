909ed675502abc56eb7464c761148f19e713625f
Vikasbehl05 <88477915+Vikasbehl05@users.noreply.github.com>
2026-06-24 22:05:39 +0530

    Add: plus-one - submission-5

---
+++ Data Structures & Algorithms/plus-one/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int[] plusOne(int[] digits) {
+        for (int j = digits.length - 1; j >= 0; j--) {
+            if (digits[j] < 9) {
+                digits[j]++;
+                return digits;
+            }
+            digits[j] = 0;
+        }
+
+        int[] newdigits = new int[digits.length + 1];
+        newdigits[0] = 1;
+        return newdigits;
+    }
+}
